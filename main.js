44a51009033c6e08862d620d619c826d2b3aef84
Jekiny <jekaorp@gmail.com>
2024-06-18 12:14:27 -0400

    Lesson 3
    Lesson 3.2/3.3

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-let wallet = 50;
- 
-let leftPocket = 200;
+// //lesson 3.2
+
+/*const name = prompt('Whats your name');
+let age = prompt('Сколько тебе лет?');
+let city = prompt('from which city you are ');
+
+
+alert (`Your name : ${name}. \n You are from ${city}. \n Your age is ${age}.`);
+*/
+
+//lesson3.3
+ let someNumber = {a:1,b:0,c:3,d:6,e:9};
+ let stringNumber = String(someNumber);
+
+ console.log(`${stringNumber.a} ${stringNumber.b} ${stringNumber.c} ${stringNumber.d} ${stringNumber.e}`);
+
+ /*let someNumber = {a: 1, b: 0, c: 3, d: 6, e: 9};
+ let stringNumber = String(someNumber.a) + " " + String(someNumber.b) + " " + String(someNumber.c) + " " + String(someNumber.d) + " " + String(someNumber.e);
+ console.log(stringNumber);*/ 
