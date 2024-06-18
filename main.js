981426ee0050893d82e3af32fccfab75b6895901
Jekiny <jekaorp@gmail.com>
2024-06-18 13:53:31 -0400

    lesson3
    different tasks

--- main.js
+++ main.js
@@ -7,13 +7,31 @@ let city = prompt('from which city you are ');
 
 alert (`Your name : ${name}. \n You are from ${city}. \n Your age is ${age}.`);
 */
+// let information = {
+//     name: "Kostya",
+//     age: 33,
+//     city: "Kyiv"
+// };
+
+// alert (`There are some information about user:\nUser age:${information.age}\n User name:${information.name}\n User hometown${information.city}`);
+
 
 //lesson3.3
- let someNumber = {a:1,b:0,c:3,d:6,e:9};
- let stringNumber = String(someNumber);
+//  let someNumber = {a:1,b:0,c:3,d:6,e:9};
+//  let stringNumber = String(someNumber);
 
- console.log(`${stringNumber.a} ${stringNumber.b} ${stringNumber.c} ${stringNumber.d} ${stringNumber.e}`);
+//  console.log(typeof some);
 
- /*let someNumber = {a: 1, b: 0, c: 3, d: 6, e: 9};
- let stringNumber = String(someNumber.a) + " " + String(someNumber.b) + " " + String(someNumber.c) + " " + String(someNumber.d) + " " + String(someNumber.e);
- console.log(stringNumber);*/ 
+//lesson3.3
+//  let someNumber = {a: 1, b: 0, c: 3, d: 6, e: 9};
+//  let stringNumber = String(someNumber.a) + " " + String(someNumber.b) + " " + String(someNumber.c) + " " + String(someNumber.d) + " " + String(someNumber.e);
+//  console.log(stringNumber);
+
+//lesson3.3
+// let someNumbers = 10369;
+
+//  let numberBeString = String(someNumbers);
+// console.log(numberBeString[0],numberBeString[1],numberBeString[2],numberBeString[3],numberBeString[4],typeof numberBeString ) ;
+
+
+
