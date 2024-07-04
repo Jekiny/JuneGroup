ecc6b323591752f335a22942c72a5a9a85e2e377
Jekiny <jekaorp@gmail.com>
2024-07-04 11:30:39 -0400

    7.2

--- lesson7/main.js
+++ lesson7/main.js
@@ -1,18 +1,87 @@
-function clousure(){
-let someNum = 0;
+//замыкание 
 
-function addSomeNum(){
-    return someNum += 4;
+// function clousure(){
+// let someNum = 0;
+
+// function addSomeNum(){
+//     return someNum += 4;
+// }
+
+// return addSomeNum; 
+
+// }
+
+// const clousureFunc =  clousure();
+
+// console.log(clousureFunc());
+// console.log(clousureFunc());
+// console.log(clousureFunc());
+// console.log(clousureFunc());
+// console.log(clousureFunc());
+
+// function clousure(someNum){
+   
+    
+
+
+// 7.2
+
+function sumNum(a){
+    return function (b){
+        return a + b ;
+    }
+
 }
 
-return addSomeNum; 
+console.log(sumNum(2)(2))
+    
+// 7.3 error
 
-}
 
-let clousureFunc =  clousure();
+// function userNum() {
+//     let userInput = prompt('Введите число');
 
-console.log(clousureFunc());
-console.log(clousureFunc());
-console.log(clousureFunc());
-console.log(clousureFunc());
-console.log(clousureFunc());
+//     if (userInput === null) {
+
+//         alert('Вы отменили ввод');
+
+//     }  else if (parseInt(userInput) > 100) {
+//         alert('Введенное число больше  100');
+
+//     }
+
+//     else if (parseInt(userInput) < 100) {
+//         alert('Введенное число меньше 100');
+
+//     } else {
+//         alert('Вы отменили');
+//     }
+// }
+
+// userNum();
+
+// function userNum(){
+
+//     let userInput = prompt('Введите число');
+
+//     for (let userInput = 0; userInput < 10 ; userInput++){
+//         if (userInput === null) {
+
+//                     alert('Вы отменили ввод');
+            
+//                 }  else if (parseInt(userInput) > 100) {
+//                     alert('Введенное число больше  100');
+            
+//                 }
+            
+//                 else if (parseInt(userInput) < 100) {
+//                     alert('Введенное число меньше 100');
+            
+//                 } else {
+//                     alert('Вы отменили');
+//                 }
+
+
+// }
+
+// userNum()
