9d00a2a759863bf04f5141dae09773710746688c
Jekiny <jekaorp@gmail.com>
2024-07-18 08:28:23 -0400

    Update main.js

--- 10mainlesson/main.js
+++ 10mainlesson/main.js
@@ -49,3 +49,21 @@ const userId = {
 for(const keyUserId in userId){
     console.log (`${keyUserId}: ${userId[keyUserId]}`)
 }
+
+
+
+// 10.3
+const Bofa = {
+    clients:[
+        {accountId:34502 , openDate: '03.11.2024', email: "johncraford@gmail.com", mobilephone: 2137612763},
+        {accountId:12302 , openDate: '05.05.2021', email: "johncraford@gmail.com", mobilephone: 165271908},
+        {accountId:11023 , openDate: '06.11.2019', email: "alexa@gmail.com", mobilephone: 3471236781},
+        {accountId:34012 , openDate: '01.01.2020', email: "petr@gmail.com", mobilephone: 98712315},
+        {accountId:98172 , openDate: '07.04.2023', email: "lio@gmail.com", mobilephone: 87965713},
+    ],
+    clientId(id){
+        return this.clients.find(accountsid =>
+         accountsid.accountId === id)
+    }
+}
+console.log(Bofa.clientId(34502))
